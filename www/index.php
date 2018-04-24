0113a275947dc5511f062ddeceada4e66a5be5ca
rangerdong <844199574@qq.com>
2018-04-24 22:21:58 +0800

    mysql8.0问题处理

--- www/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("mysql", "root", "123456", "lnmp");
-if (!$conn) {
-   echo "Error: Unable to connect to MySQL." . PHP_EOL;     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;     exit;
-}
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL; echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-
